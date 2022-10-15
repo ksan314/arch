041c625d25e4caa2626c38527de57cb73b53bf4c
ksan314 <91560218+ksan314@users.noreply.github.com>
2022-10-15 14:29:18 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -284,6 +284,148 @@ usermod -aG libvirt "$userName"
 fi
 
 
+# configure mullvadvpn if package is installed
+mullvadExists=$(pacman -Qqs mullvad)
+if [ -z "$mullvadExists" ]
+then
+	sleep 1
+else
+	# ask user if they want to configure mullvadvpn
+	echo -e "\n\n\n"
+	while true
+	do
+	read -rp $'\n'"Would you like to configure MullvadVPN? [Y/n] " mullvadConfig
+    	mullvadConfig=${mullvadConfig:-Y}
+    	if [ "$mullvadConfig" == Y ] || [ "$mullvadConfig" == y ] || [ "$mullvadConfig" == yes ] || [ "$mullvadConfig" == YES ] || [ "$mullvadConfig" == Yes ]
+    	then
+        	mullvadConfig=true
+        	read -rp $'\n'"Are you sure you DO want to configure MullvadVPN? [Y/n] " mullvadconfigConfirm
+        	mullvadconfigConfirm=${mullvadconfigConfirm:-Y}
+        	case $mullvadconfigConfirm in
+            	[yY][eE][sS]|[yY]) break;;
+            	[nN][oO]|[nN]);;
+            	*);;
+        	esac
+			REPLY=
+		else
+        	mullvadConfig=false
+        	read -rp $'\n'"Are you sure you DO NOT want to configure MullvadVPN? [Y/n] " mullvadconfigConfirm
+        	mullvadconfigConfirm=${mullvadconfigConfirm:-Y}
+        	case $mullvadconfigConfirm in
+            	[yY][eE][sS]|[yY]) break;;
+            	[nN][oO]|[nN]);;
+            	*);;
+			esac
+        	REPLY=
+    	fi
+	done
+
+	if [ "$mullvadConfig" == true ]
+	then
+		# update server list
+		mullvad relay update
+	
+		# get mullvadvpn account number
+		while true
+		do
+		read -rp $'\n'"Enter your mullvadvpn account number: " mullvadAccount
+			if [ -z "$mullvadAccount" ]
+			then
+				echo -e "\nYou must enter an account number\n"
+			else
+				read -rp $'\n'"Are you sure you want to use the account number \"$mullvadAccount\"? [Y/n] " mullvadaccountConfirm
+				mullvadaccountConfirm=${mullvadaccountConfirm:-Y}
+				case $mullvadaccountConfirm in
+					[yY][eE][sS]|[yY]) break;;
+					[nN][oO]|[nN]);;
+					*);;
+				esac
+				REPLY=
+			fi
+		done
+
+		# get mullvadvpn server country
+		echo -e "\n\n\n"
+		mapfile -t serverCountries < <(mullvad relay list | grep -Eo '\([a-z][a-z]\)' | grep -Eo '[a-z][a-z]')
+		PS3="Enter the number for the mullvadvpn server country you want to use: "
+		select serverCountry in "${serverCountries[@]}"
+		do
+			if (( REPLY > 0 && REPLY <= "${#serverCountries[@]}" ))
+			then
+				read -rp $'\n'"Are you sure you want to select the server country \"$serverCountry\"? [Y/n] " servercountryConfirm
+				servercountryConfirm=${servercountryConfirm:-Y}
+				case $servercountryConfirm in
+					[yY][eE][sS]|[yY]) break;;
+					[nN][oO]|[nN]);;
+					*);;
+				esac
+				REPLY=
+			else
+				echo -e "\nInvalid option. Try another one\n"
+				sleep 2
+				REPLY=
+			fi
+		done
+
+		# get mullvadvpn server city
+		echo -e "\n\n\n"
+		mapfile -t serverCities < <(mullvad relay list | grep -Eo "$serverCountry-[a-z][a-z][a-z]" | grep -Eo '[a-z][a-z][a-z]$')
+		PS3="Enter the number for the mullvadvpn server city you want to use: "
+		select serverCity in "${serverCities[@]}"
+		do
+			if (( REPLY > 0 && REPLY <= "${#serverCities[@]}" ))
+			then
+				read -rp $'\n'"Are you sure you want to select the server city \"$serverCity\"? [Y/n] " servercityConfirm
+				servercityConfirm=${servercityConfirm:-Y}
+				case $servercityConfirm in
+					[yY][eE][sS]|[yY]) break;;
+					[nN][oO]|[nN]);;
+					*);;
+				esac
+				REPLY=
+			else
+				echo -e "\nInvalid option. Try another one\n"
+				sleep 2
+				REPLY=
+			fi
+		done
+
+		# login to mullvadvpn
+		mullvad account login "$mullvadAccount"
+
+		# if login successful, configure mullvadvpn
+		mullvadLogin=$(mullvad account get | grep "$mullvadAccount")
+		if [ -n "$mullvadLogin" ]
+		then
+			# set server location
+			mullvad relay set location "$serverCountry" "$serverCity"
+
+			# set the protocol to wireguard
+			mullvad relay set tunnel-protocol wireguard
+
+			# enable LAN access
+			mullvad lan set allow
+    
+			# block ads, trackers, and malware
+			mullvad dns set default --block-ads --block-malware --block-trackers
+    
+			# set auto-connect
+			mullvad auto-connect set on
+    
+			# connect to mullvadvpn
+			mullvad connect
+    
+			# always require vpn
+			mullvad always-require-vpn set on
+		
+		else
+			printf "\e[1;31m\nLogin to Mullvad not successful\n\e[0m"
+			exit
+		fi
+	fi
+fi
+
+
 
 
 
