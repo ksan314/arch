ef56b5d0171c90b9e9a2f21e4f081a487261ef41
ksan314 <91560218+ksan314@users.noreply.github.com>
2022-10-22 15:11:55 -0500

    Update and rename pipewire-volume.sh to pipewire-max-volume.sh

--- files/scripts/pipewire/pipewire-max-volume.sh
+++ files/scripts/pipewire/pipewire-max-volume.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-maxVolume=100
+maxVolume=95
 defaultSink=$(pactl get-default-sink)
 currentVolume=$(pactl get-sink-volume "$defaultSink" | grep -Eio '[0-9]*%' | head -n 1 | grep -Eio '[0-9]*')
 
