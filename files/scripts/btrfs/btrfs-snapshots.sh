29dc18660548e2c44c34f3b0bd09ba9654f2c67f
ksan314 <91560218+ksan314@users.noreply.github.com>
2022-10-24 15:55:58 -0500

    Update btrfs-snapshots.sh

--- files/scripts/btrfs/btrfs-snapshots.sh
+++ files/scripts/btrfs/btrfs-snapshots.sh
@@ -13,31 +13,40 @@ maxsnapshotCount=NUMBEROFSNAPSHOTS
 # clean up old snapshots
 ########################
 
-# get number of existing snapshots
+# delete old root snapshots
+while true
+do
+# get number of existing root snapshots
 rootsnapshotCount=$(btrfs subvolume list -o /.snapshots | grep 'root' | grep -c 'TIMELINE')
+	if [ "$rootsnapshotCount" -ge "$maxsnapshotCount" ]
+	then
+		# get name of oldest root snapshot
+		oldestrootSnapshot=$(btrfs subvolume list -o /.snapshots | grep 'root' | grep 'TIMELINE' | head -n 1 | grep -Eo 'snapshots[[:print:]]*$')
+
+		# delete oldest root snapshot
+		btrfs subvolume delete "/.$oldestrootSnapshot"
+	else
+		break
+	fi
+done
+
+
+# delete old home snapshots
+while true
+do
+# get number of existing home snapshots
 homesnapshotCount=$(btrfs subvolume list -o /.snapshots | grep 'home' | grep -c 'TIMELINE')
+	if [ "$homesnapshotCount" -ge "$maxsnapshotCount" ]
+	then
+		# get name of oldest home snapshot
+		oldesthomeSnapshot=$(btrfs subvolume list -o /.snapshots | grep 'home' | grep 'TIMELINE' | head -n 1 | grep -Eo 'snapshots[[:print:]]*$')
 
-
-# delete oldest root snapshot
-if [ "$rootsnapshotCount" -ge "$maxsnapshotCount" ]
-then
-	# get name of oldest root snapshot
-	oldestrootSnapshot=$(btrfs subvolume list -o /.snapshots | grep 'root/TIMELINE' | head -n 1 | grep -Eo 'snapshots[[:print:]]*$')
-	
-	# delete oldest root snapshot
-	btrfs subvolume delete "/.$oldestrootSnapshot"
-fi
-
-
-# delete oldest home snapshot
-if [ "$homesnapshotCount" -ge "$maxsnapshotCount" ]
-then
-	# get name of oldest home snapshot
-	oldesthomeSnapshot=$(btrfs subvolume list -o /.snapshots | grep 'home/TIMELINE' | head -n 1 | grep -Eo 'snapshots[[:print:]]*$')
-	
-	# delete oldest home snapshot
-	btrfs subvolume delete "/.$oldesthomeSnapshot"
-fi
+		# delete oldest home snapshot
+		btrfs subvolume delete "/.$oldesthomeSnapshot"
+	else
+		break
+	fi
+done
 
 
 
