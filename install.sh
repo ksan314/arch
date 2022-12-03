c405b21a67a671e9c80a607c7cb1c8d12e3aa750
ksan314 <91560218+ksan314@users.noreply.github.com>
2022-12-03 12:32:48 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -239,7 +239,7 @@ sleep 2
 printf "\e[1;32m\nInstalling packages needed for installation\n\e[0m"
 sleep 2
 
-pacman -S archlinux-keyring ca-certificates neofetch upower virt-what
+pacman -S archlinux-keyring ca-certificates neofetch virt-what
 
 
 
@@ -268,7 +268,7 @@ fi
 
 
 # check if installing on a laptop
-laptopInstall=$(upower --show-info)
+laptopInstall=$(neofetch --battery_display bar | grep -io battery)
 if [ -z "$laptopInstall" ]
 then
     laptopInstall=false
